df90b80bbee33f3a938c9de2460c71f38bb1ce4c
koshi.beats <musicxanzoro@gmail.com>
2024-03-08 09:10:36 +0100

    use datatable to render all customers

--- components/Customer.tsx
+++ components/Customer.tsx
@@ -1,27 +1,47 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+
 import { SubmitHandler, useForm } from "react-hook-form";
 import LoadCustomers from "./LoadCustomers";
 
 export default function EditCustomer() {
-  return (
-    <div className="bg-white dark:bg-gray-900">
-      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <div className="table w-full ...">
-          <div className="table-header-group ...">
-            <div className="table-row">
-              <div className="table-cell text-left ...">UUID</div>
-              <div className="table-cell text-left ...">First Name</div>
-              <div className="table-cell text-left ...">Last Name</div>
-            </div>
+
+  const [mydata, setData] = useState([]);
+
+  
+  const fetchUserData = async () => {
+    const { data } = await axios.get("./ja.json");
+  
+    setData(data.customers);
+    /*  {mydata.map((customer) => (
+          <div>
+            <div className="table-row">{customer.uuid}</div>
           </div>
-          <div className="table-row-group">
-            <div className="table-row">
-              <LoadCustomers></LoadCustomers>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+        ))}*/
+    
+  };
+  useEffect(() => {
+    fetchUserData();
+   
+  }, []);
+
+  return (<>
+
+
+
+
+    <DataTable value={mydata} tableStyle={{ minWidth: '50rem' }}>
+    <Column field="firstname" header="First Name"></Column>
+    <Column field="lastname" header="Last Name"></Column>
+    <Column field="uuid" header="UUID"></Column>
+    
+</DataTable>
+</>
+        
+        
+
+    
   );
 }
--- components/LoadCustomers.tsx
+++ components/LoadCustomers.tsx
@@ -3,23 +3,23 @@ import React, { useState, useEffect } from "react";
 
 export default function LoadCustomers() {
   const [mydata, setData] = useState([]);
-  const [myDataArroved, setDataArrived] = useState(false);
-  console.log(mydata.map);
-  const fetchUserData = async () => {
-    const { data } = await axios.get("http://localhost:8080/api/customers");
 
-    setData(data);
-    console.log("cock");
+  
+  const fetchUserData = async () => {
+    const { data } = await axios.get("./ja.json");
+
+    setData(data.customers);
+    
   };
 
   useEffect(() => {
     fetchUserData();
-    setDataArrived(true);
+   
   }, []);
 
   return (
     <div>
-      {myDataArroved &&
+      {
         mydata.map((customer) => (
           <div>
             <h2>{customer.uuid}</h2>
